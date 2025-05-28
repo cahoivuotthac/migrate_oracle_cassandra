998939f3bab3ec46a979d255fce06e6590e1cc2f
cahoivuotthac <thuyhienphanthi2004@gmail.com>
2025-05-29 06:02:19 +0700

    ✨feature: add new SQL queries for data retrieval and enhance existing table structures in Oracle and Cassandra schemas

--- sql/cassandra_schema.sql
+++ sql/cassandra_schema.sql
@@ -44,47 +44,52 @@ CREATE TABLE IF NOT EXISTS danhmuc_sanpham (
 );
 
 -- Data model tables 
-CREATE TABLE IF NOT EXISTS products_by_category (
-    ten_danh_muc text,
-    ma_san_pham text,
-    ten_san_pham text,
-    the_loai tinyint,
-    gia bigint,
-    PRIMARY KEY (ten_danh_muc, ma_san_pham)
-);
+-- CREATE TABLE IF NOT EXISTS chi_tiet_san_pham_theo_ma_sp ( 
+--     ma_san_pham TEXT,
+--     ten_san_pham TEXT,
+--     gia INT,
+--     the_loai INT,
+--     ten_danh_muc TEXT,
+--     ten_thuoc_tinh TEXT,
+--     gia_tri_thuoc_tinh TEXT,
+--     PRIMARY KEY (ma_san_pham, ten_thuoc_tinh)
+-- );
 
-CREATE TABLE IF NOT EXISTS chi_tiet_hoa_don_by_ma_hoa_don (
-    ma_hoa_don text,
-    ma_san_pham text,
-    ten_san_pham text,
-    so_luong int,
-    thanh_tien decimal,
-    gia decimal,
-    the_loai text,
-    PRIMARY KEY (ma_hoa_don, ma_san_pham)
-);
+CREATE TABLE IF NOT EXISTS chi_tiet_hoa_don_theo_ma_kh (
+    ma_khach_hang TEXT,
+    ma_hoa_don TEXT,
+    ma_san_pham TEXT,
+    so_luong INT,
+    thanh_tien bigint,
+    tong_tien bigint,
+    ngay_tao TIMESTAMP,
+    phuong_thuc_thanh_toan TEXT,
+    ma_nhan_vien TEXT,
+    PRIMARY KEY ((ma_khach_hang), ngay_tao, ma_hoa_don)
+) WITH CLUSTERING ORDER BY (ngay_tao DESC);
 
-CREATE TABLE IF NOT EXISTS daily_revenue_by_branch (
+CREATE TABLE IF NOT EXISTS doanh_thu_moi_ngay_theo_ma_cn (
     ma_chi_nhanh int,
     ngay date,
     tong_tien bigint,
     PRIMARY KEY ((ma_chi_nhanh), ngay)
-);
+) WITH CLUSTERING ORDER BY (ngay DESC);
 
-CREATE TABLE IF NOT EXISTS product_ratings_by_branch (
+CREATE TABLE IF NOT EXISTS kho_sp_theo_ma_cn (
     ma_chi_nhanh int,
     ma_san_pham text,
     ten_san_pham text,
-    tong_danh_gia int,
-    tong_sao float,
-    avg_sao float,
-    PRIMARY KEY ((ma_chi_nhanh), ma_san_pham)
+    tinh_trang text, 
+    tong_sl_danh_gia int,
+    tong_sl_da_ban int,
+    tong_sl_ton_kho int,
+    PRIMARY KEY ((ma_chi_nhanh), ma_san_pham, tong_sl_ton_kho)
 );
 
-CREATE TABLE IF NOT EXISTS top_products_by_branch (
-    ma_chi_nhanh int,
-    so_luong_da_ban int,
-    ma_san_pham text,
-    ten_san_pham text,
-    PRIMARY KEY ((ma_chi_nhanh), so_luong_da_ban, ma_san_pham)
-) WITH CLUSTERING ORDER BY (so_luong_da_ban DESC);
+CREATE TABLE IF NOT EXISTS sl_khach_hang_moi_ngay_theo_ma_cn (
+  ma_chi_nhanh int,
+  ngay date,
+  so_luong_khach_hang int,
+  PRIMARY KEY ((ma_chi_nhanh), ngay)
+) WITH CLUSTERING ORDER BY (ngay DESC); 
+
--- sql/oracle_queries.sql
+++ sql/oracle_queries.sql
@@ -1,1 +1,66 @@
--- 
+-- For Cassandra's data model tables 
+-- chi_tiet_san_pham_theo_ma_sp
+-- SELECT 
+--     sp."MaSanPham",
+--     sp."TenSanPham",
+--     sp."Gia",
+--     sp."TheLoai",
+--     dmsp."TenDanhMuc",
+--     ttsp."TenThuocTinh",
+--     ttsp."GiaTriThuocTinh"
+-- FROM BTL1."SanPham" sp, BTL1."DanhMuc_SanPham" dmsp, BTL1."ThuocTinh_SanPham" ttsp
+-- WHERE sp."MaSanPham" = dmsp."MaSanPham" AND sp."MaSanPham" = ttsp."MaSanPham"
+
+-- chi_tiet_hoa_don_theo_ma_kh
+SELECT 
+  hd."MaKhachHang",
+  cthd."MaHoaDon",
+  cthd."MaSanPham",
+  cthd."SoLuong",
+  cthd."ThanhTien",
+  hd."TongTien",
+  hd."NgayTao",
+  hd."PhuongThucThanhToan",
+  hd."MaNhanVien"
+FROM BTL1."ChiTietHoaDon" cthd 
+  JOIN BTL1."HoaDon" hd ON cthd."MaHoaDon" = hd."MaHoaDon"
+  RIGHT JOIN BTL1."KhachHang" kh ON HD."MaKhachHang" = KH."MaKhachHang"
+ORDER BY BTL1."MaHoaDon", BTL1."NgayTao" DESC;
+
+-- doanh_thu_moi_ngay_theo_ma_cn 
+SELECT 
+  cn."MaChiNhanh",
+  TRUNC(hd."NgayTao") AS Ngay,
+  SUM(hd."TongTien") AS TongTien
+FROM "HoaDon" hd 
+  RIGHT JOIN "NhanVien" nv ON hd."MaNhanVien" = nv."MaNhanVien"
+  JOIN "ChiNhanh" cn ON cn."MaChiNhanh" = nv."MaChiNhanh"
+GROUP BY cn."MaChiNhanh", TRUNC(hd."NgayTao")
+ORDER BY Ngay DESC;
+
+-- kho_sp_theo_ma_cn
+SELECT 
+  KSPQ."MaChiNhanh",
+  KSPQ."MaSanPham",
+  SP."TenSanPham",
+  KSPQH."TinhTrang",
+  KSPQH."TongSoLuongDanhGia",
+  KSPQH."TongSoLuongDaBan",
+  KSPQ."SoLuong"
+FROM "KhoSanPham_QLBanHang" kspqh, "KhoSanPham_QLKho" kspq, "SanPham" sp
+WHERE KSPQ."MaSanPham" = KSPQH."MaSanPham" AND SP."MaSanPham" = KSPQ."MaSanPham"
+ORDER BY KSPQ."MaSanPham", KSPQ."SoLuong";
+
+-- sl_khach_hang_moi_ngay_theo_ma_cn
+SELECT 
+  NV."MaChiNhanh",
+  TRUNC(HD."NgayTao") AS Ngay,
+  COUNT(DISTINCT HD."MaKhachHang") AS SoLuongKhachHang
+FROM "NhanVien" NV
+  JOIN "HoaDon" HD ON NV."MaNhanVien" = HD."MaNhanVien"
+GROUP BY NV."MaChiNhanh", TRUNC(HD."NgayTao")
+ORDER BY Ngay DESC;
+
+
+
+
