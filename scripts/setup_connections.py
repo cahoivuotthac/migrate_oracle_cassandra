e89dd8e6da5be9fac1853ffacb7fb0339a4612d7
cahoivuotthac <thuyhienphanthi2004@gmail.com>
2025-05-28 18:21:22 +0700

    ✨feature: update connection setup to load configurations from YAML file

--- scripts/setup_connections.py
+++ scripts/setup_connections.py
@@ -1,45 +1,51 @@
 import os
+import yaml
 from airflow import settings
 from airflow.models import Connection
 from airflow.utils.db import provide_session
 
 @provide_session
 def create_connection(conn_id, conn_type, host, schema, login, password, port, session=None):
-    conn = Connection(
-        conn_id=conn_id,
-        conn_type=conn_type,
-        host=host,
-        schema=schema,
-        login=login,
-        password=password,
-        port=port
-    )
-    session.add(conn)
-    session.commit()
-    print(f"Connection {conn_id} created.")
+	conn = Connection(
+		conn_id=conn_id,
+		conn_type=conn_type,
+		host=host,
+		schema=schema,
+		login=login,
+		password=password,
+		port=port
+	)
+	session.add(conn)
+	session.commit()
+	print(f"Connection {conn_id} created.")
 
 def setup_connections():
-    # Oracle connection
-    create_connection(
-        conn_id='oracle_BTL2',
-        conn_type='oracle',
-        host='26.93.36.133',
-        schema='orcl',
-        login='BTL1',
-        password='password',
-        port=1521
-    )
+	config_path = os.path.join(os.path.dirname(__file__), '..', 'config', 'connections.yaml')
+	with open(config_path, 'r') as file:
+		config = yaml.safe_load(file)
+		
+	oracle_config = config['connections']['oracle_db']
+	cassandra_config = config['connections']['cassandra_db']
+ 
+	create_connection(
+		conn_id=oracle_config['conn_id'],
+		conn_type=oracle_config['oracle'],
+		host=oracle_config['host'],
+		schema=oracle_config['schema'],
+		login=oracle_config['login'],
+		password=oracle_config['password'],
+		port=oracle_config['port']
+	)
 
-    # Cassandra connection
-    create_connection(
-        conn_id='cassandra_BTL2',
-        conn_type='cassandra',
-        host='localhost',
-        schema='etl_data',
-        login='',
-        password='',
-        port=9042
-    )
+	create_connection(
+		conn_id=cassandra_config['conn_id'],
+		conn_type=cassandra_config['conn_type'],
+		host=cassandra_config['host'],
+		schema=cassandra_config['schema'],
+		login='',
+		password='',
+		port=cassandra_config['port']
+	)
 
 if __name__ == "__main__":
-    setup_connections()
+	setup_connections()
